e1844d400af2c5501e8ceb6f5159b71f09d695a5
Anjali Trace <anjalitrace@gluster-node-2.localdomain>
2019-09-09 13:50:00 +0100

    Each person has a random favourite drink. Can set favourite drinks or get a table of favourite drinks.

--- brIW1.py
+++ brIW1.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import random
 
 args = sys.argv
 help = f"""HELP: brIW helps you collect drinks rounds. To start, just enter
@@ -19,7 +20,9 @@ menu = """
     [2] Get all drinks
     [3] Add people
     [4] Add drinks
-    [5] Save & Exit
+    [5] Get favourite drinks
+    [6] Set favourite drinks
+    [E] Save & Exit
 """
 welcome = "Welcome to BrIW v0.1!"
 people_file = "./people.txt"
@@ -40,7 +43,6 @@ def ask_to_continue(message):
 def update_list_file(list_file, updated_list):
     pass
 
-
 def read_list_file(file_name):
     assert file_name != "", "No file supplied."
 
@@ -48,34 +50,68 @@ def read_list_file(file_name):
     with open(file_name,"rt",newline="\n") as file:
         for line in file:
             if line[:-1] != "":
-                items.append(line[:-1])
+                items.append(line[:-1].capitalize())
     
     if len(items) == 0:
         ask_to_continue(f"Empty file supplied: {file_name}")
 
     return items
 
-def calculate_width(rows):
+def get_person_id(name):
+    return people.index(name)+1
+
+def get_drink_id(drink):
+    return drinks.index(drink)+1
+
+def get_person_from_id(name_id):
+    return people[name_id-1]
+
+def get_drink_from_id(drink_id):
+    return drinks[drink_id-1]
+
+def set_preference(name_id, drink_id):
+    preferences[name_id] = drink_id
+
+def calculate_column_width(rows):
     longest_item = 0
     for item in rows:
         if len(item) > longest_item:
             longest_item = len(item)
     
-    if longest_item+4 > 20:
-        return longest_item+4
+    if longest_item+6 > 20:
+        return longest_item+6
     return 20
 
-def make_row(text, table_width):
-    return "| {}{} |\n".format(text.capitalize(), " "*(table_width-4-len(text)))
+def make_row(text, table_width, use_index=True, index=""):
+    if use_index:
+        return "| {} {}{} |\n".format(index, text, " "*(table_width-6-len(text)))
+    else:
+       return "| {}{} |\n".format(text, " "*(table_width-4-len(text)))
 
-def print_as_table(header, rows):
-    table_width = calculate_width(rows)
-    table_break = "+{}+\n".format("-"*(table_width-2))
-    text = table_break + make_row(header, table_width) + table_break
-    for item in rows:
-        text += make_row(item, table_width)
-    text += table_break
-    print(text)
+def make_table_break(table_width):
+    return "+{}+\n".format("-"*(table_width-2))
+
+def _print_as_table(header, rows, table_width, use_index):
+    text = make_table_break(table_width) + make_row(header, table_width, use_index, index=" ") + make_table_break(table_width)
+    for index in range(len(rows)):
+        text += make_row(rows[index], table_width, use_index, index=index+1)
+    text += make_table_break(table_width)
+    return text
+
+def print_1column_table(header, rows):
+    return _print_as_table(header, rows, calculate_column_width(rows), True)
+
+def print_preferences(preferences):
+    names_width = calculate_column_width(people)-6
+    drinks_width = calculate_column_width(drinks)-6
+    header = "Person{} | Fav Drink {}".format(" "*(names_width-7), " "*(drinks_width-11))
+    # header = make_row(" ", "Name", names_width)[-2] + make_row(" ", "Fav drink", drinks_width)
+    rows = []
+    for name_id in preferences:
+        name = get_person_from_id(name_id)
+        drink = get_drink_from_id(preferences[name_id])
+        rows.append("{}{} | {}{}".format(name, " "*(names_width-len(name)-1), drink, " "*(drinks_width-len(drink)-1)))
+    return _print_as_table(header, rows, calculate_column_width(rows), False)
 
 def take_list_input(list_to_update):
     while True:
@@ -83,10 +119,10 @@ def take_list_input(list_to_update):
         if item == "":
             break
 
-        if len(item) > max_table_width-4:
-            print(f"Item not added - cannot be more than {max_table_width-4} characters. Try again.")
+        if len(item) > max_table_width-6:
+            print(f"Item not added - cannot be more than {max_table_width-6} characters. Try again.")
         else: 
-            list_to_update.append(item)
+            list_to_update.append(item.capitalize())
             os.system("clear")
             print(f"{item} successfully added. Enter another or hit ENTER to return to menu.")
     os.system("clear")
@@ -100,6 +136,11 @@ start_num_of_people = len(people)
 drinks = read_list_file(drinks_file)
 start_num_of_drinks = len(drinks)
 
+preferences = {}
+for index in range(len(people)):
+    # Assign a random favourite drink for now
+    preferences[index] = random.randint(0,len(drinks)-1)
+
 os.system("clear")
 print(welcome)
 
@@ -109,20 +150,42 @@ while True:
     os.system("clear")
     if len(command) == 0:
         print("Expected input, none provided. ")
+
     elif command == "1":
-        print_as_table("People", people)
+        print(print_1column_table("People", people))
         input("Hit ENTER to return to menu.")
+
     elif command == "2":
-        print_as_table("Drinks", drinks)
+        print(print_1column_table("Drinks", drinks))
         input("Hit ENTER to return to menu.")
-    elif  command == "3":
+
+    elif command == "3":
         print("Please enter a name to add. Hit enter when done to add another. ")
         people = take_list_input(people)
+
     elif command == "4":
         print("Please enter a drink to add: ")
         drinks = take_list_input(drinks)
+
     elif command == "5":
+        print(print_preferences(preferences))
+        input("Hit ENTER to return to menu.")
+
+    elif command == "6":
+        print(print_1column_table("People", people))
+        print("To set a favourite drink, enter the id of a person.")
+        name_id = int(input("Enter an id: "))
+        print(print_1column_table("Drinks", drinks))
+        print("Now enter the id of the favourite drink.")
+        drink_id = int(input("Enter an id: "))
+        set_preference(name_id, drink_id)
+        os.system("clear")
+        print(f"{get_person_from_id(name_id)}'s favourite drink is now {get_drink_from_id(drink_id)}.\n")
+        input("Hit ENTER to return to menu.")
+
+    elif command.lower() == "e":
         break
+
     else: 
         print("Unrecognised command. ")
     os.system("clear")
