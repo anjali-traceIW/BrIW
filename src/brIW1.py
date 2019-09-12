c8486fecf5a5427b5c6deecacaf6740c64d6de6b
Anjali Trace <anjalitrace@gluster-node-2.localdomain>
2019-09-12 15:35:49 +0100

    Removed some unused code.

--- src/brIW1.py
+++ src/brIW1.py
@@ -38,43 +38,6 @@ drinks_file = "data/drinks.txt"
 preferences_file = "data/preferences.txt"
 max_table_width = 90
 
-# def update_list_file(list_file, updated_list):
-#     # Overwrites whole file (TODO: not this)
-#     with open(list_file,"w") as file:
-#         for item in updated_list:
-#             file.write(f"{item}\n")
-
-# def update_dict_file(dict_file, updated_dict):
-#     with open(dict_file,"w") as file:
-#         for key in updated_dict:
-#             file.write(f"{key}:{updated_dict[key]}\n")
-
-# def read_list_file(file_name):
-#     assert file_name != "", "No file supplied."
-
-#     items = []
-#     with open(file_name,"rt",newline="\n") as file:
-#         for line in file:
-#             if line[:-1] != "":
-#                 items.append(line[:-1].capitalize())
-    
-#     if len(items) == 0:
-#         ask_to_continue(f"Empty file supplied: {file_name}")
-
-#     return items
-
-# def read_dict_integer_file(file_name):
-#     # Expects integer key and value pair
-#     lines = read_list_file(file_name)
-#     new_dict = {}
-#     for line in lines:
-#         key, value = line.split(":")
-#         if value == "":
-#             new_dict[int(key)] = ""
-#         else:
-#             new_dict[int(key)] = int(value)
-#     return new_dict
-
 def get_person_id(name):
     return people.index(name)+1
 
@@ -89,49 +52,6 @@ def get_drink_from_id(drink_id):
 
 def set_preference(name_id, drink_id):
     preferences[name_id] = drink_id
-
-# def calculate_column_width(rows):
-#     longest_item = 0
-#     for item in rows:
-#         if len(item) > longest_item:
-#             longest_item = len(item)
-    
-#     if longest_item+6 > 20:
-#         return longest_item+6
-#     return 20
-
-# def make_row(text, table_width, use_index=True, index=""):
-#     if use_index:
-#         return "| {} {}{} |\n".format(index, text, " "*(table_width-6-len(text)))
-#     else:
-#        return "| {}{} |\n".format(text, " "*(table_width-4-len(text)))
-
-# def make_table_break(table_width):
-#     return "+{}+\n".format("-"*(table_width-2))
-
-# def _print_as_table(header, rows, table_width, use_index):
-#     text = make_table_break(table_width) + make_row(header, table_width, use_index, index=" ") + make_table_break(table_width)
-#     for index in range(len(rows)):
-#         text += make_row(rows[index], table_width, use_index, index=index+1)
-#     text += make_table_break(table_width)
-#     return text
-
-# def print_single_column_table(header, rows):
-#     return _print_as_table(header, rows, calculate_column_width(rows), True)
-
-# def print_people_drinks(preferences, headers=("Person", "Fav Drink")):
-#     names_width = calculate_column_width(people)-6
-#     drinks_width = calculate_column_width(drinks)-6
-#     header = "{}{} | {} {}".format(headers[0], " "*(names_width-1-len(headers[0])), headers[1], " "*(drinks_width-len(headers[1])))
-#     rows = []
-#     for name_id in preferences:
-#         name = get_person_from_id(name_id)
-#         if preferences[name_id] != "":
-#             drink = get_drink_from_id(preferences[name_id])
-#         else:
-#             drink = ""
-#         rows.append("{}{} | {}{}".format(name, " "*(names_width-len(name)-1), drink, " "*(drinks_width-len(drink)-1)))
-#     return _print_as_table(header, rows, calculate_column_width(rows), False)
 
 def take_list_input(list_to_update, update_preferences=False):
     updated = False
@@ -157,8 +77,6 @@ people = people_file.get_people_from_file()
 
 drinks_file = FileManager.DrinksFileManager(drinks_file)
 drinks = drinks_file.get_drinks_from_file()
-
-# preferences = read_dict_integer_file(preferences_file)
 
 updated_people, updated_drinks, updated_preferences = False, False, False
 
