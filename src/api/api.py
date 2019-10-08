23decb538caeccfb71b0da0bd1e93326b454242d
Anjali Trace <anjali.trace@infinityworks.com>
2019-10-08 08:58:46 +0100

    Added missing functionality endpoints to API and refactored DbMangager to actually work.

--- src/api/api.py
+++ src/api/api.py
@@ -7,43 +7,81 @@ from flask import Flask, jsonify, request, Response, render_template
 
 app = Flask(__name__)
 
+# ============== ROUNDS ==============
+
 @app.route("/rounds", methods=["GET"])
 def get_all_rounds(RoundsDbManager=RoundsDbManager):
-
-    people = PeopleFileManager("data/people.csv").get_people_from_file()
-    drinks = DrinksFileManager("data/drinks.csv").get_drinks_from_file()
-
-    rounds_data_manager = RoundsDbManager()
-    rounds = rounds_data_manager.get_all_rounds()
+    rounds = RoundsDbManager.get_all_rounds()
     print(rounds)
 
     return jsonify([round.make_json_obj() for round in rounds])
 
+@app.route("/round", methods=["GET"])
+def get_round(RoundsDbManager=RoundsDbManager):
+    round_id = request.args.get("round_id")
+
+    round = RoundsDbManager.get_round(round_id)
+
+    return jsonify(round)
+
 @app.route("/rounds", methods=["POST"])
 def create_a_round(RoundsDbManager=RoundsDbManager):
     data = request.get_json()
-    print(data)
     round = Round(data["owner"], data["time_started"], data["active"])
     round.orders = data["orders"]
-    print(round)
 
     try:
-        round_id = RoundsDbManager.add_round(round)
+        round_id = RoundsDbManager.create_round(round)
     except Exception as e:
         return Response(response=e,status=500)
 
     return Response(response=f"Successfully added round id {round_id}.",status=200)
+
+# ============== PEOPLE ==============
 
 @app.route("/people", methods=["GET"])
 def get_all_people(PeopleDbManager=PeopleDbManager):
     people = PeopleDbManager.get_all_people()
     return jsonify([person.to_json() for person in people])
 
+@app.route("/person", methods=["GET"])
+def get_person(PeopleDbManager=PeopleDbManager):
+    person_id = request.args.get("person_id")
+    person = PeopleDbManager.get_person(person_id)
+    return jsonify(person)
+
+@app.route("/person", methods=["POST"])
+def add_person(PeopleDbManager=PeopleDbManager):
+    data = request.get_json()
+    person = Person(data["name"], data["favourite_drink"])
+    new_person_id = PeopleDbManager.create_person(person)
+    return Response(response=f"Successfully created person id {new_person_id}", status=200)
+
+# ============== DRINKS ==============
+
+@app.route("/drinks", methods=["GET"])
+def get_all_drinks(DrinksDbManager=DrinksDbManager):
+    drinks = DrinksDbManager.get_all_drinks()
+    return jsonify([drink.to_json() for drink in drinks])
+
+@app.route("/drink", methods=["GET"])
+def get_drink(DrinksDbManager=DrinksDbManager):
+    drink_id = request.args.get("drink_id")
+    drink = DrinksDbManager.get_drink(drink_id)
+    return jsonify(drink)
+
+@app.route("/drink", methods=["POST"])
+def add_drink(DrinksDbManager=DrinksDbManager):
+    data = request.get_json()
+    drink = Drink(data["name"], data["temperature"])
+    new_drink_id = DrinksDbManager.create_drink(drink)
+    return Response(response=f"Successfully created drink id {new_drink_id}", status=200)
+
+# ============== PAGES ==============
+
 @app.route("/pages/drinks", methods=["GET"])
 def get_drinks_html(DrinksDbManager=DrinksDbManager):
-
-    drinks_data_manager = DrinksDbManager()
-    drinks = drinks_data_manager.get_all_drinks()
+    drinks = DrinksDbManager.get_all_drinks()
     
     # Produce the HTML
     html_document = """
@@ -76,5 +114,5 @@ def person_form():
 
 if __name__ == "__main__":
     print("Starting server")
-    app.run(host="localhost", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
     
--- src/cls/DbManager.py
+++ src/cls/DbManager.py
@@ -37,28 +37,31 @@ class DbManager:
     
 class PeopleDbManager(DbManager):
         
-    def get_all_people(self, drinks):
-        people = People()
+    def get_all_people():
+        people = []
         try:
             results = DbManager.execute_select("SELECT person_name, drink_name FROM people LEFT JOIN drinks ON people.favourite_drink_id=drinks.drink_id")
+            for row in results:
+                # print(row)
+                people.append(Person(row[0], Drink(row[1])))
         except Exception as e:
             # TODO: improve this sad message
             print("Something went wrong...")
             print(e)
-        for row in results:
-            print(row)
-            people.add_person(Person(row[0], drinks.get_drink(row[1])))
         return people
 
-    def get_person(self, person_id):
+    def get_person(person_id):
+        person = Person()
         query = "SELECT person_name, drink_name FROM people JOIN drinks ON favourite_drink_id = drink_id WHERE person_id=%s"
         try: 
             results = DbManager.execute_select(query, (person_id))
+            person = Person(results[0], Drink(results[1]))
         except Exception as e:
             print(e)
-        return Person(results[0], Drink(results[1]))
+        return person
 
-    def create_person(self, person):
+    def create_person(person):
+        new_person_id = -1
         query = "INSERT INTO people (person_name, favourite_drink_id) VALUES (%s, (SELECT drink_id FROM drinks WHERE drink_name = %s))"
         try:
             new_person_id = DbManager.execute_update_or_insert(query, (person.name, person.favourite_drink.name))
@@ -69,21 +72,30 @@ class PeopleDbManager(DbManager):
 
 class DrinksDbManager(DbManager):
 
-    def get_all_drinks(self):
-        drinks = Drinks()
+    def get_all_drinks():
+        drinks = []
         try:
             results = DbManager.execute_select("SELECT drink_name FROM drinks")
             for row in results:
                 print(row)
-                drinks.add_drink(Drink(row[0]))
+                drinks.append(Drink(row[0]))
         except Exception as e:
             # TODO: improve this sad message
             print("Something went wrong...")
             print(e)
-
         return drinks
 
-    def create_drink(self, drink):
+    def get_drink(drink_id):
+        drink = Drink()
+        try: 
+            result = DbManager.execute_select("SELECT drink_name FROM drinks WHERE drink_id=%s", (drink_id))[0]     # Only one result to be returned.
+            drink = Drink(result[0])
+        except Exception as e:
+            print(e)
+        return drink
+
+    def create_drink(drink):
+        new_drink_id = -1
         query = "INSERT INTO drinks (drink_name) VALUES (%s)"
         try: 
             new_drink_id = DbManager.execute_update_or_insert(query, (drink.name))
@@ -92,52 +104,43 @@ class DrinksDbManager(DbManager):
         return new_drink_id
 
 
-class RoundsDbManager:
+class RoundsDbManager(DbManager):
 
-    def get_all_rounds(self):
+    def get_all_rounds():
         rounds = []
         try: 
             results = DbManager.execute_select("SELECT round_id, active, time_started, person_name FROM rounds JOIN people on person_id=owner_id;")
             for row in results:
-                # print(row)
                 round = Round(owner=row[3], time_started=row[2], active=(row[1] == 1))
-                orders = self.get_orders_for_a_round(row[0])
-                # print(f"Orders: {orders}")
+                orders = DbManager.get_orders_for_a_round(row[0])
                 round.orders = orders
-                # print(round)
                 rounds.append(round)
         except Exception as e:
             print(e)
         return rounds
 
-    def get_round(self, round_id):
+    def get_round(round_id):
+        round = Round()
         query = "SELECT active, time_started, person_name FROM rounds JOIN people on person_id=owner_id WHERE round_id=%s;"
         try: 
             result = DbManager.execute_select(query, (round_id))[0] # Returns a tuple (single result)
+            round = Round(result[2], result[1], result[0])
         except Exception as e:
             print(e)
-        return Round(result[2], result[1], result[0])
+        return round
 
-    # def update_round(self, updated_round):
-    #     query = f"UPDATE rounds SET active={updated_round.active}"
-    #     try:
-    #         return DbManager.execute_update_or_insert(query)
-    #     except Exception as e:
-    #         print(e)
-
-    def create_round(self, new_round):
+    def create_round(new_round):
+        round_id = -1
         query = f"INSERT INTO rounds (active, time_started, owner_id) VALUES (%s, CURRENT_TIMESTAMP, (SELECT person_id FROM people WHERE person_name=%s))"
         param = (new_round.get_active_as_int(), new_round.owner)
-        
-        round_id = DbManager.execute_update_or_insert(query, param)
-        print(f"Inserted round {round_id}")
-
-        for person, drink in new_round.orders.items():
-            order_id = RoundsDbManager.add_order_to_round(round_id, (person, drink))
-            
+        try:
+            round_id = DbManager.execute_update_or_insert(query, param)
+            # print(f"Inserted round {round_id}")
+            for person, drink in new_round.orders.items():
+                order_id = RoundsDbManager.add_order_to_round(round_id, (person, drink))
         return round_id
 
-    def get_orders_for_round(self, round_id):
+    def get_orders_for_round(round_id):
         orders = {}
         # Could this be shorter? I don't think this could be shorter.
         query = "SELECT person_name, drink_name FROM drinks JOIN (SELECT person_name, drink_id FROM people JOIN (SELECT person_id, drink_id FROM orders WHERE round_id = %s) AS order_info ON people.person_id=order_info.person_id) AS person_drink_id ON drinks.drink_id=person_drink_id.drink_id"
@@ -149,7 +152,8 @@ class RoundsDbManager:
             orders[row[0]] = row[1]  # This is stupid?
         return orders
 
-    def create_order_for_round(self, round_id, order):
+    def create_order_for_round(round_id, order):
+        new_order_id = -1
         # print(f"order: {order}")
         query="INSERT INTO orders (person_id, drink_id, round_id) VALUES ((SELECT person_id FROM people WHERE person_name=%s), (SELECT drink_id FROM drinks WHERE drink_name=%s), %s)"
         param=(order[0], order[1], round_id)
